89d12949a249984e0a7fb32016f4f5b7d5ce1d8d
Sergey Kovalev <dipdeepcode@mail.ru>
2025-04-12 18:25:02 +0300

    init commit

---
+++ src/main/java/ru/ddc/oauthclient/OauthClientApplication.java
@@ -0,0 +1,13 @@
+package ru.ddc.oauthclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OauthClientApplication.class, args);
+    }
+
+}
