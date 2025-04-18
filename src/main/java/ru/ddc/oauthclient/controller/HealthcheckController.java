34fc700583340a6472af1298b69f8ac0658b5875
Sergey Kovalev <dipdeepcode@mail.ru>
2025-04-18 22:42:52 +0300

    feat :: changed workflow trigger and iam-tool port

--- src/main/java/ru/ddc/oauthclient/controller/HealthcheckController.java
+++ src/main/java/ru/ddc/oauthclient/controller/HealthcheckController.java
@@ -1,6 +1,5 @@
 package ru.ddc.oauthclient.controller;
 
-import lombok.Getter;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,19 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthcheckController {
 
     @GetMapping(value = "/health")
-    public ResponseEntity<HealthcheckResponse> healthCheck() {
-        HealthcheckResponse response = new HealthcheckResponse(200, "OK");
-        return ResponseEntity.ok(response);
-    }
-
-    @Getter
-    public static class HealthcheckResponse {
-        int code;
-        String status;
-
-        public HealthcheckResponse(int code, String status) {
-            this.code = code;
-            this.status = status;
-        }
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok("OK");
     }
 }
