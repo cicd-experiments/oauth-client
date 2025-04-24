a135c6f8422231203dd4cadd6005ff17eee361e0
svkovalev <svkovalev@bank-hlynov.ru>
2025-04-24 09:58:46 +0300

    test :: Local Logout
    https://docs.spring.io/spring-security/reference/servlet/oauth2/login/logout.html#configure-local-logout

--- src/main/java/ru/ddc/oauthclient/configuration/SecurityConfig.java
+++ src/main/java/ru/ddc/oauthclient/configuration/SecurityConfig.java
@@ -6,16 +6,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
-    private final ClientRegistrationRepository clientRegistrationRepository;
     /**
      * Security configuration class.
      * @param http HttpSecurity object for configuration.
@@ -29,21 +25,10 @@ public class SecurityConfig {
 
         http.oauth2Login(Customizer.withDefaults());
 
-        http.logout(logout -> logout.logoutSuccessHandler(oidcLogoutSuccessHandler()));
-
         http.authorizeHttpRequests(ex -> ex
                 .requestMatchers("/", "/index.html", "/favicon.ico").permitAll()
                 .anyRequest().authenticated());
 
         return http.build();
-    }
-
-    private LogoutSuccessHandler oidcLogoutSuccessHandler() {
-        OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler =
-                new OidcClientInitiatedLogoutSuccessHandler(this.clientRegistrationRepository);
-
-        oidcLogoutSuccessHandler.setPostLogoutRedirectUri("https://dipdeepcode.ru");
-
-        return oidcLogoutSuccessHandler;
     }
 }
