e8c4c7f56bbaffdf51e9a4df377249ebf7f81f02
svkovalev <svkovalev@bank-hlynov.ru>
2025-04-28 14:58:25 +0300

    test :: logoutSuccessHandler

--- src/main/java/ru/ddc/oauthclient/configuration/SecurityConfig.java
+++ src/main/java/ru/ddc/oauthclient/configuration/SecurityConfig.java
@@ -6,12 +6,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+    private final ClientRegistrationRepository clientRegistrationRepository;
+
     /**
      * Security configuration class.
      * @param http HttpSecurity object for configuration.
@@ -25,17 +30,21 @@ public class SecurityConfig {
 
         http.oauth2Login(Customizer.withDefaults());
 
-        http.logout(logout -> logout
-                .invalidateHttpSession(true)
-                .clearAuthentication(true)
-                .deleteCookies("JSESSIONID", "AUTH_SESSION_ID", "KC_RESTART",
-                        "KEYCLOAK_IDENTITY", "KEYCLOAK_SESSION"));
-
+        http.logout(c -> c.logoutSuccessHandler(oidcLogoutSuccessHandler()));
 
         http.authorizeHttpRequests(ex -> ex
                 .requestMatchers("/", "/index.html", "/favicon.ico").permitAll()
                 .anyRequest().authenticated());
 
         return http.build();
+    }
+
+    private LogoutSuccessHandler oidcLogoutSuccessHandler() {
+        OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler =
+                new OidcClientInitiatedLogoutSuccessHandler(clientRegistrationRepository);
+
+        oidcLogoutSuccessHandler.setPostLogoutRedirectUri("{baseUrl}");
+
+        return oidcLogoutSuccessHandler;
     }
 }
